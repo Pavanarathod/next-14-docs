0beea6df13e51ea2a095ad04bb525566c7170d07
Pavanarathod <edrocullen13@gmail.com>
2024-01-16 15:17:53 +0530

    docs nextjs.com

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Roboto, Lusitana } from 'next/font/google';
+
+export const roboto = Roboto({
+  weight: '400',
+  subsets: ['greek'],
+});
+
+export const lusy = Lusitana({
+  weight: '400',
+  subsets: ['latin'],
+});
